b238c076a27a4c44ba5367c22a4aa7d79963e09f
Gene Liverman <gene@technicalissues.us>
2016-10-11 14:12:30 -0400

    Initial module commit.

---
+++ manifests/init.pp
@@ -0,0 +1,56 @@
+# Creates a simple nginx.conf for a reverse proxy setup
+# nginx_proxy {
+#   $upstreams = [
+#     {
+#       title   => 'old_backend_http',
+#       servers => [
+#         '127.0.0.1:8081',
+#       ],
+#     },
+#     {
+#       title   => 'old_backend_https',
+#       servers => [
+#         '127.0.0.1:8444',
+#       ],
+#     },
+#     {
+#       title     => 'new_backend_https',
+#       lb_method => 'ip_hash',
+#       servers   => [
+#         '127.0.0.1:9444',
+#         '127.0.0.1:10444',
+#       ],
+#     },
+#   ],
+# }
+class nginx_proxy (
+  $access_log                = '/var/log/nginx/access.log',
+  $config_file               = '/etc/nginx/nginx.conf',
+  $docroot                   = '/usr/share/nginx/html',
+  $dynamic_modules           = '/usr/share/nginx/modules/*.conf',
+  $error_log                 = '/var/log/nginx/error.log',
+  $locations                 = [],
+  $mime_include              = '/etc/nginx/mime.types',
+  $pid_file                  = '/run/nginx.pid',
+  $port_http                 = 80,
+  $port_https                = 443,
+  $required_packages         = ['nginx'],
+  $server_name              = '_',
+  $ssl_certificate           = '/etc/pki/tls/certs/localhost.crt',
+  $ssl_certificate_key       = '/etc/pki/tls/private/localhost.key',
+  $ssl_session_cache         = 'shared:SSL:1m',
+  $ssl_session_timeout       = '10m',
+  $ssl_ciphers               = 'HIGH:!aNULL:!MD5',
+  $ssl_prefer_server_ciphers = 'on',
+  $upstreams                 = [],
+  $user                      = 'nginx',
+  $worker_connections        = 1024,
+  $worker_processes          = 'auto',
+){
+  file { $config_file:
+    ensure  => file,
+    content => template('nginx_proxy/nginx.conf.erb'),
+    owner   => $user,
+    require => Package[$required_packages],
+  }
+}
